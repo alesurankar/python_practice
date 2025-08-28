6b8a7f475e161597b9da548364fc8b5310fea8eb
ales2026 <ale.urankar@gmail.com>
2025-08-28 09:15:09 +0200

    adding photos

--- ImageEditor/photoEditor.py
+++ ImageEditor/photoEditor.py
@@ -3,3 +3,12 @@ import os
 
 path = './imgs'
 pathOut = '/editedImgs'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+
+    edit = img.filter(ImageFilter.SHARPEN)
+
+    clean_name = os.path.splitext(filename)[0]
+
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
