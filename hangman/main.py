b56ad9fe5205f9f69a00ac764931716158b17a90
ales2026 <ale.urankar@gmail.com>
2025-08-29 21:11:09 +0200

    checking characters

--- hangman/main.py
+++ hangman/main.py
@@ -15,5 +15,19 @@ def hangman():
     alphabet = set(string.asci_uppercase)
     used_letters = set()  #what the user has guessed
 
+    #getting user input
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('You have already used that character. Please try again.')
+
+    else:
+        print('Invalid character. Please try again.')
+
+
 user_input = input('Type something:')
 print(user_input)
