c1493d433088aed1cedbed6091a2ce91ed18279d
ales2026 <ale.urankar@gmail.com>
2025-08-29 21:23:23 +0200

    working guessing game

--- hangman/main.py
+++ hangman/main.py
@@ -3,31 +3,42 @@ from words import words
 import string
 
 def get_valid_word(words):
-    word = random.chouce(words) #randomly chooses something from the list
+    word = random.choice(words) #randomly chooses something from the list
     while '_' in word or ' ' in word:
         word = random.choice(words)
 
-    return word
+    return word.upper()
 
 def hangman():
     word = get_valid_word(words)
     word_letters = set(word) # letters in the word
-    alphabet = set(string.asci_uppercase)
+    alphabet = set(string.ascii_uppercase)
     used_letters = set()  #what the user has guessed
 
     #getting user input
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    while len(word_letters) > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have used these letters: ', ' '.join(used_letters))
 
-    elif user_letter in used_letters:
-        print('You have already used that character. Please try again.')
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word:', ' '.join(word_list))
 
-    else:
-        print('Invalid character. Please try again.')
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
 
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
 
-user_input = input('Type something:')
-print(user_input)
+        else:
+            print('Invalid character. Please try again.')
+
+    # gets here when len(word_letters) == 0
+    # stop game properly
+    print(f'Congratulations! You guessed the word {word}!')
+
+hangman()
